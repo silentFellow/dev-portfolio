cdae9046cf106fcf7ae11aec6f81819e833ea577
silentFellow <gowtham.sri+github@zohomail.in>
2025-01-21 09:40:07 +0530

    Add hero section

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,11 @@
+import Hero from "@/components/Hero";
+
 const Home = () => {
   return (
-    <main className="bg-[rgb(33,33,33)] text-[#f5f5f5] flex flex-col justify-center items-center h-screen">
-      Just chilling
+    <main className="relative bg-black-100 col center overflow-hidden mx-auto sm:px-10 px-5">
+      <div className="max-w-7xl w-full">
+        <Hero />
+      </div>
     </main>
   );
 };
