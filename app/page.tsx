43d9b7fe776abe74315eea6d117efd545e1e1d26
silentFellow <gowtham.sri+github@zohomail.in>
2025-01-21 06:32:30 +0530

    initial commit

---
+++ app/page.tsx
@@ -0,0 +1,9 @@
+const Home = () => {
+  return (
+    <main className="bg-[rgb(33,33,33)] text-[#f5f5f5] flex flex-col justify-center items-center h-screen">
+      Just chilling
+    </main>
+  );
+};
+
+export default Home;
