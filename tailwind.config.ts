1bc1c99b7f68e118c1527922d5665e96ecbee10b
silentFellow <gowtham.sri+github@zohomail.in>
2025-01-23 14:51:44 +0530

    Update magic-button

--- tailwind.config.ts
+++ tailwind.config.ts
@@ -79,6 +79,7 @@ export default {
       },
       animation: {
         spotlight: "spotlight 2s ease .75s 1 forwards",
+        shimmer: "shimmer 2s linear infinite",
       },
       keyframes: {
         spotlight: {
@@ -89,6 +90,14 @@ export default {
           "100%": {
             opacity: "1",
             transform: "translate(-50%,-40%) scale(1)",
+          },
+        },
+        shimmer: {
+          from: {
+            backgroundPosition: "0 0",
+          },
+          to: {
+            backgroundPosition: "-200% 0",
           },
         },
       },
